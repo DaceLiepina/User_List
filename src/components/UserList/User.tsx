2ebbce061585d32d325c4fbe64d09f2e6233d640
Dace Liepina <dace.liepina77@gmail.com>
2025-10-01 19:46:37 +0200

    User Card Theme

--- src/components/UserList/User.tsx
+++ src/components/UserList/User.tsx
@@ -18,25 +18,24 @@ const User: FC<{ user: IUser }> = ({ user: { id, name, username, email, } }) => 
       className={`col-12 col-md-6 col-lg-4`}
       key={v4()}
     >
-      <div className={`card h-100 shadow-sm ${
-        isDark ? "bg-dark text-light" : "bg-light text-dark"
-      }`}>
-        <div className="card-header bg-info text-black">
-          <h5 className="mb-3">{name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">@{username}</h6>
-          <p className="bi bi-envelope me-2">
-            <strong> Email: </strong> {email}
-          </p>
-        </div>
-        <div className="card-footer bg-light">
-          <Link
-            to={`/users/${id}`}
-            className="btn btn-sm btn-outline-info w-100"
-          >
-            View Profile
-          </Link>
-        </div>
-      </div>
+    <div className="card h-100 shadow-sm">
+  <div className={`card-header ${isDark ? "bg-dark text-light" : "bg-light text-dark"}`}>
+    <h5 className="mb-3">{name}</h5>
+    <h6 className="card-subtitle mb-2">@{username}</h6>
+    <p className="bi bi-envelope me-2">
+      <strong>Email:</strong> {email}
+    </p>
+  </div>
+
+  <div className={`card-footer ${isDark ? "bg-dark text-light" : "bg-light text-dark"}`}>
+    <Link
+      to={`/users/${id}`}
+      className="btn btn-sm btn-outline-info w-100"
+    >
+      View Profile
+    </Link>
+  </div>
+</div>
     </div>
   );
 };
